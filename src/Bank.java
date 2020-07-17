9c8272c96a2168e954a3f44a8037fda564470bba
tatsuya828828 <kkjy011894@gmail.com>
2020-07-17 20:39:52 +0900

    [finish]14-05

--- src/Bank.java
+++ src/Bank.java
@@ -7,7 +7,7 @@ public class Bank {
 	}
 
 	public void setName(String newName) {
-		if(newName.length() <= 3) {
+		if(newName.length() < 3) {
 			throw new IllegalArgumentException("名前が不正です");
 		}
 		this.name = newName;
